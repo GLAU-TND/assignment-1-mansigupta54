a765e5298beb8bbc968a1d92ef85e91b1040d3df
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2020-02-21 23:31:12 +0530

    implement the ContactADT in ContactList

---
+++ src/defination/ContactList.java
@@ -0,0 +1,25 @@
+package defination;
+
+import contactADT.ContactADT;
+
+public class ContactList<E> implements ContactADT<E> {
+    @Override
+    public E add() {
+        return null;
+    }
+
+    @Override
+    public E view() {
+        return null;
+    }
+
+    @Override
+    public E search() {
+        return null;
+    }
+
+    @Override
+    public E delete() {
+        return null;
+    }
+}
