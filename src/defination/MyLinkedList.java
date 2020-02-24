6b35bb62d28b1c76f45ca9b4e68494fdb5dedf5a
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2020-02-24 19:54:10 +0530

    getter and setter of the field

--- src/defination/MyLinkedList.java
+++ src/defination/MyLinkedList.java
@@ -5,5 +5,22 @@ import Helper.Node;
         private Node<T> head;           //It is the beginning of linked list
         private Node<T> iter;           //It is used to iterate the list
 
+        public Node<T> getHead() {
+            return head;
+        }        //getter for head
+
+        public void setHead(Node<T> node) {
+            head = node;
+            iter = head;
+        }   //setter for head
+
+        public Node<T> getIter() {
+            return iter;
+        }                //Getter for iter
+
+        public void setIter(Node<T> iter) {
+            this.iter = iter;
+        }  //Setter for iter
+
 
     }
