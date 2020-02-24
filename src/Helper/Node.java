14f6bf47ae4e09e70dc7c6d74240aa2de91af144
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2020-02-24 19:35:38 +0530

    field of node class

---
+++ src/Helper/Node.java
@@ -0,0 +1,10 @@
+package Helper;
+
+
+    //Generic Node class
+    public class Node<T> {
+        private T data;         //Data of node
+        private Node<T> next;   //Self referential data
+
+
+    }
