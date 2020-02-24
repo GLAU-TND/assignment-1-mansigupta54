6a478f96d9c8e6bf8caaa39c6093bfac9ef30e70
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2020-02-24 19:36:45 +0530

    getter setter of data and self referential data

--- src/Helper/Node.java
+++ src/Helper/Node.java
@@ -6,5 +6,25 @@ package Helper;
         private T data;         //Data of node
         private Node<T> next;   //Self referential data
 
+        //Getter for data
+        public T getData() {
+            return data;
+        }
+
+        //Setter for data
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        //Getter for self referential data
+        public Node<T> getNext() {
+            return next;
+        }
+
+        //Setter for self referential data
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
 
     }
