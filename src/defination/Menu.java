7f5d998b09b0a502a1dd6aef66cf6959b27f53c4
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2020-02-24 21:08:34 +0530

    field and the constructor

--- src/defination/Menu.java
+++ src/defination/Menu.java
@@ -6,5 +6,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
     public class Menu {
+        private MyLinkedList<Person> linkedList;
+        private Scanner scan;
+
+        public Menu() {
+            scan = new Scanner(System.in);
+            linkedList = new MyLinkedList<>();
+        }
+
 
     }
