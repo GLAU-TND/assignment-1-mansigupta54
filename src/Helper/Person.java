d323e52afd82765ee5f5c0712763913e1b6c0a43
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2020-02-24 19:27:35 +0530

    getter setter for first name

--- src/Helper/Person.java
+++ src/Helper/Person.java
@@ -5,6 +5,17 @@ package Helper;
         private String emailID;                     //EmailId of person
         private StringBuffer phoneNumbers;          //Phone numbers of person
 
+     //Getter for first name
+        public String getFirstName() {
+            return firstName;
+        }
+
+        //Setter for first name
+        public void setFirstName(String firstName) {
+            System.out.println(firstName);
+            this.firstName = firstName;
+        }
+
 
     }
 
