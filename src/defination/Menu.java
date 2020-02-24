77690f0f71a95e246f8221b6be68b0c0f10fed07
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2020-02-24 21:10:15 +0530

    method to show the choice

--- src/defination/Menu.java
+++ src/defination/Menu.java
@@ -14,5 +14,15 @@ import java.util.Scanner;
             linkedList = new MyLinkedList<>();
         }
 
+        public int showChoice() {
+            System.out.println("Welcome to Mansi's Contact List App\n" +
+                    "Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program");
+            return scan.nextInt();
+        }
+
 
     }
