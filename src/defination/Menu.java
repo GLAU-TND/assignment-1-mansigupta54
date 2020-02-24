59336f8bad9af31efdfe4b431f76a1d5d42b95bd
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2020-02-24 21:11:07 +0530

    method to add the choice

--- src/defination/Menu.java
+++ src/defination/Menu.java
@@ -24,5 +24,56 @@ import java.util.Scanner;
             return scan.nextInt();
         }
 
+        public void addChoice() {
+            Person person = new Person();
+            String name;
+            System.out.println("You have chosen to add a new contact: \n" +
+                    "Please enter the name of the Person\n");
+            scan.nextLine();
+
+            System.out.print("First Name: ");
+            name = scan.nextLine().trim();
+            person.setFirstName(name);
+
+            System.out.print("Last Name: ");
+            name = scan.nextLine().trim();
+            person.setLastName(name);
+
+            String number;
+            System.out.print("Contact Number: ");
+            number = scan.nextLine().trim();
+            person.setPhoneNumbers(number);
+
+            char c;
+            System.out.print("Would you like to add another contact number? (y/n): ");
+            c = scan.nextLine().trim().charAt(0);
+            while (c == 'y') {
+                System.out.print("Contact Number: ");
+                number = scan.nextLine().trim();
+                person.setPhoneNumbers(number);
+
+                System.out.print("Would you like to add another contact number? (y/n): ");
+                c = scan.nextLine().trim().charAt(0);
+            }
+
+
+            String email = null;
+            System.out.print("Would you like to add email address? (y/n): ");
+            c = scan.nextLine().trim().charAt(0);
+            if (c == 'y') {
+                System.out.print("Email Address: ");
+                email = scan.nextLine().trim();
+                person.setEmailID(email);
+                System.out.println("y");
+            } else {
+                person.setEmailID("");
+                System.out.println("n");
+            }
+
+            Node<Person> node =new Node<>();
+            node.setData(person);
+            linkedList.insert(node);
+        }
+
 
     }
