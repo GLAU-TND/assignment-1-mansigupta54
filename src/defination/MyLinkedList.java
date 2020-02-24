e9a63834c869147c9e9e703e49108198f8b28002
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2020-02-24 19:53:02 +0530

    field for the linkedlist

---
+++ src/defination/MyLinkedList.java
@@ -0,0 +1,9 @@
+package defination;
+import Helper.Node;
+
+    public class MyLinkedList<T extends  Comparable<T>> {
+        private Node<T> head;           //It is the beginning of linked list
+        private Node<T> iter;           //It is used to iterate the list
+
+
+    }
