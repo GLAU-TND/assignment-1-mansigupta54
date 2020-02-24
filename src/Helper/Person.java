9a689c203f3b3889e24f248fc7c30f70df29e9c5
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2020-02-24 19:25:27 +0530

    a field to store the first name,last name,email address of the person  a list to store multiple contact numbers

---
+++ src/Helper/Person.java
@@ -0,0 +1,10 @@
+package Helper;
+    public class Person  {
+        private String firstName;                   //First name of person
+        private String lastName;                    //Last name of person
+        private String emailID;                     //EmailId of person
+        private StringBuffer phoneNumbers;          //Phone numbers of person
+
+
+    }
+
